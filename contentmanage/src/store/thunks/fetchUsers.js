fe75ef43afb2dd51087553c88039bbf265878c3f
shivamk012 <shivamkoolwal14@gmail.com>
2023-07-15 17:20:28 +0530

    added a pause - dev

--- contentmanage/src/store/thunks/fetchUsers.js
+++ contentmanage/src/store/thunks/fetchUsers.js
@@ -4,8 +4,18 @@ import axios from "axios";
 const fetchUser = createAsyncThunk('user/fetch' , async() => {
     const result = await axios.get("https://api.waqi.info/feed/tokyo/?token=demo");
     
+    //DEV ONLY - pause for testing
+    await pause(1000);
+
     // this data acts as payload in action object
     return result.data.data.aqi;
 })
+
+//DEV ONLY
+const pause = (duration) => {
+    return new Promise((resolve , reject) => {
+        setTimeout(resolve , duration);
+    })
+}
 
 export {fetchUser};
